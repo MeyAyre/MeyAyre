e69365bfca749a295e4cd0f36aafd763a76ec69d
Mei'lani <meyrehay@gmail.com>
2018-04-09 09:47:17 -0700

    Fixed images for responsiveness

--- Scripts/resume.js
+++ Scripts/resume.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
-
+  $(".tab-link").click(function(){
+    var i = $(this).attr('data-tab');
+    if(i == 'tab6'){
+      $(".tab-content").css("display", "block");
+    }
+  });
 });
