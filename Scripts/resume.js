aa8b6a63c190582fbace8d56a337708ce516c6ce
Mei'lani <meyrehay@gmail.com>
2018-04-09 14:35:05 -0700

    Significant changes to site

--- Scripts/resume.js
+++ Scripts/resume.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
+  var tabcontent = $('.tabcontent');
+  var tablinks = $('.tab-link');
+
+  tabcontent.each(function(i){
+    $(this).css("display", "none");
+  });
   $(".tab-link").click(function(){
     var i = $(this).attr('data-tab');
     if(i == 'tab6'){
