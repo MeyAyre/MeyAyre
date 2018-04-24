4a52c12ae286a8f8219a7dd561c79fb22ad0f0a6
Mei'lani <meyrehay@gmail.com>
2018-04-23 17:50:01 -0700

    Made the carousel for the details

---
+++ Scripts/gallery.js
@@ -0,0 +1,10 @@
+$(document).ready( function(){
+  $('.main-gallery').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    lazyLoad: 1,
+    freeScroll: true,
+    wrapAround: true 
+  });
+});
