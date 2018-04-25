3513d3dfb001497b0f7ad0841e55b23596ed380b
Mei'lani <meyrehay@gmail.com>
2018-04-24 21:01:20 -0700

    This is the best I'm doing so let's call the gallery done

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -1,10 +1,23 @@
+//Created with Flickity
+//Find the documentation here https://flickity.metafizzy.co 
 $(document).ready( function(){
   $('.main-gallery').flickity({
     // options
     cellAlign: 'center',
     contain: true,
+    draggable: false,
     lazyLoad: 1,
     freeScroll: true,
-    wrapAround: true 
+    wrapAround: true
+  });
+
+  $('.has-pictures').click(function(){
+    $('.claire-bob').css('display', 'block');
+      // resize flickity after being shown
+     $('.main-gallery').flickity('resize');
+    $('#details-placeholder').css('display', 'none');
+  });
+  $('#close-button').click(function(){
+    $('#mobile-details').css('display', 'none');
   });
 });
