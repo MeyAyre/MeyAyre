af0b17e800b654bd5e9794ab8978f4a50f3e21ef
Mei <meyrehay@gmail.com>
2018-05-08 14:28:42 -0700

    Very close to being done

---
+++ Scripts/kidsdesign.js
@@ -0,0 +1,44 @@
+//Gem moving code was adopted from https://codepen.io/jonathan/pen/wixFp by Jonathan Marzullo
+//Using GSAP
+
+$(document).ready( function(){
+
+  //Gem moving code
+  var totalElements = 0;
+  var elementAnimationDelay = 0;
+  var elementCountUp = 0;
+
+  function elementMoveDown(elementItem) {
+  if(elementCountUp != totalElements) {
+    elementCountUp++;
+  } else {
+    elementAnimationDelay = 0; //no delay after initial run
+  }
+
+  TweenMax.to(elementItem, 1.3, {
+    delay: elementAnimationDelay,
+    y: "+=20px",
+    yoyo:true,
+    repeat:-1,
+    ease: Power2.easeOut//,
+    //onCompleteParams:[elementItem],
+    //onComplete: elementMoveUp
+    });
+  }
+  function initPhoneHovering() {
+  $(".gems > img[id]").each(function(){
+    totalElements++;
+    elementAnimationDelay += 0.4;
+
+    var targetElement = $("#element-"+totalElements);
+
+    //init animation
+    elementMoveDown(targetElement);
+    });
+  }
+
+  initPhoneHovering();
+
+  //Easter Eggs
+
+});
