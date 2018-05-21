d308767f500402e86afb01fe5bf68107a75adaba
Mei'lani <meyrehay@gmail.com>
2018-05-21 07:27:32 -0700

    Just a rough build

---
+++ Scripts/ft.js
@@ -0,0 +1,13 @@
+//Created with Flickity
+//Find the documentation here https://flickity.metafizzy.co
+$(document).ready( function(){
+  $('.main-gallery').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    draggable: false,
+    lazyLoad: 1,
+    freeScroll: true,
+    wrapAround: true
+  });
+});
